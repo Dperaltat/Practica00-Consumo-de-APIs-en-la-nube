21f34c2384db93bc4836a28814d482c2899cb97c
Daniel Peralta <peraltadaniel069@gmail.com>
2020-04-06 22:03:06 -0500

    Creando JS

--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,18 @@
+$(document).ready(()=>{
+    $('#buscador').on('enviar', (e) => {
+        let busctext = $('#buscTXT').val();
+        getPelis(busctext);
+        e.preventDefault();
+    });
+});
+
+function getPelis(busctext){
+    axios.get('http://www.omdbapi.com?s='+busctext).then((respuesta) =>{
+    console.log(respuesta);
+    })
+    .catch((err) =>{
+        console.log(err);
+    });
+}
+
 //id 813e50a8
