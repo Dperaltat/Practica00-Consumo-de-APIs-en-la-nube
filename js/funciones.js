6b46fa0407edaf4da2e2207df5b6f04ca826ec06
Daniel Peralta <peraltadaniel069@gmail.com>
2020-04-06 22:36:53 -0500

    JS funcional

--- js/funciones.js
+++ js/funciones.js
@@ -1,14 +1,30 @@
 $(document).ready(() => {
-    $('#buscador').on('enviar', (e) => {
-        let busctext = $('#buscTXT').val();
-        getPelis(busctext);
+    $('#buscador').on('submit', (e) => {
+        let buscTXT = $('#buscTXT').val();
+        getPelis(buscTXT);
         e.preventDefault();
     });
 });
 
-function getPelis(busctext){
-    axios.get('http://www.omdbapi.com?s='+busctext).then((respuesta) => {
-    console.log(respuesta);
+function getPelis(buscTXT){
+    axios.get('https://www.omdbapi.com?s='+buscTXT+"&apikey=813e50a8")
+    .then((respuesta) => {
+        console.log(respuesta);
+        let peliculas = respuesta.data.Search;
+        let output = '';
+        $.each(peliculas, ( index, movie) => {
+            output += ` 
+                <div class="col-md-3">
+                    <div class="well text-center">
+                        <img src="${movie.Poster}">
+                        <h5>${movie.title}</h5>
+                        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#"> Detalles de la Película</a>
+                    </div>
+                </div>
+            `;
+        });
+        
+        $('#peliculas').html(output);
     })
     .catch((err) =>{
         console.log(err);
