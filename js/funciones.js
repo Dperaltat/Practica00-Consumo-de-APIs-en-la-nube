f98d779682f166b77199d6736cff6796c6633b0f
Daniel Peralta <peraltadaniel069@gmail.com>
2020-04-08 00:36:20 -0500

    Busqueda ID

--- js/funciones.js
+++ js/funciones.js
@@ -13,31 +13,7 @@ function getPelis(buscTexT){
         var peliculas = respuesta.data.Search;
         var output = '';
         $.each(peliculas, (index, movie) => {
-            output += ` 
-                <div class="col-md-3">
-                    <div class="well text-center">
-                        <img src="${movie.Poster}">
-                        <h5>${movie.Title}</h5>
-                        <a onclick="peliSelect('${movie.imdbID}')" class="btn btn-success" href="#"> Detalles</a>
-                    </div>
-                </div>
-            `;
-        });
-        $('#peliculas').html(output);
-    })
-    .catch((err) =>{
-        console.log(err);
-    });
-}
-
-function getPelisID(buscTexT){
-    axios.get('https://www.omdbapi.com?i='+buscTexT+"&apikey=813e50a8")
-    .then((respuesta) => {
-        console.log(respuesta);
-        var peliculas = respuesta.data.Search;
-        var output = '';
-        $.each(peliculas, (index, movie) => {
-            output += ` 
+            output += `
                 <div class="col-md-3">
                     <div class="well text-center">
                         <img src="${movie.Poster}">
@@ -72,10 +48,10 @@ function getPelicula(){
                     <img src="${movie.Poster}" class="thumbnail">
                 </div>
                 <div class="col-md-8">
-                    <h2>${movie.Title}"</h2>
+                    <h2>${movie.Title}</h2>
                     <ul class="list-group">
                         <li class="list-group-item"><strong>Género:</strong> ${movie.Genre}</li>
-                        <li class="list-group-item"><strong>Publicada:</strong> ${movie.Released}</li>
+                        <li class="list-group-item"><strong>Publicación:</strong> ${movie.Released}</li>
                         <li class="list-group-item"><strong>Clasificación:</strong> ${movie.Rated}</li>
                         <li class="list-group-item"><strong>Calificación IMDB:</strong> ${movie.imdbRating}</li>
                         <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
@@ -93,10 +69,56 @@ function getPelicula(){
                     <a href="index.html" class="btn btn-default">Inicio</a>
                     </hr>
                 </div>
-            </div>  
+            </div>
         `;
-        
+
         $('#peli').html(output);
+    })
+    .catch((err) =>{
+        console.log(err);
+    });
+}
+
+$(document).ready(() => {
+    $('#buscadorid').on('submit', (e) => {
+        var buscID = $('#buscID').val();
+        getPelisID(buscID);
+        e.preventDefault();
+    });
+});
+
+function getPelisID(id){
+    axios.get('https://www.omdbapi.com?i='+id+"&apikey=813e50a8")
+    .then((respuesta) => {
+        console.log(respuesta);
+        var movieID = respuesta.data;
+        var output = `
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="${movieID.Poster}" class="thumbnail">
+                </div>
+                <div class="col-md-8">
+                    <h2>${movieID.Title}</h2>
+                    <ul class="list-group">
+                        <li class="list-group-item"><strong>Género:</strong> ${movieID.Genre}</li>
+                        <li class="list-group-item"><strong>Publicación:</strong> ${movieID.Released}</li>
+                        <li class="list-group-item"><strong>Clasificación:</strong> ${movieID.Rated}</li>
+                        <li class="list-group-item"><strong>Calificación IMDB:</strong> ${movieID.imdbRating}</li>
+                        <li class="list-group-item"><strong>Director:</strong> ${movieID.Director}</li>
+                        <li class="list-group-item"><strong>Escritor:</strong> ${movieID.Writer}</li>
+                        <li class="list-group-item"><strong>Actores:</strong> ${movieID.Actors}</li>
+                        <li class="list-group-item"><strong>Código:</strong> ${movieID.imdbID}</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="well">
+                    <h3>Trama</h3>
+                    ${movieID.Plot}
+                </div>
+            </div>
+        `;
+        $('#peliculasid').html(output);
     })
     .catch((err) =>{
         console.log(err);
